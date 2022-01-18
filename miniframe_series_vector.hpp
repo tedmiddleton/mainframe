15d5cdac0120ac9f739bb90b8492d2e8dcfe2dd6
Ted Middleton <middleton.ted@gmail.com>
2022-01-17 19:08:05 -0800

    Adding copyright notices (BSL-1.0)

--- miniframe_series_vector.hpp
+++ miniframe_series_vector.hpp
@@ -1,3 +1,9 @@
+
+//          Copyright Ted Middleton 2022.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef INCLUDED_miniframe_series_vector_h
 #define INCLUDED_miniframe_series_vector_h
 
