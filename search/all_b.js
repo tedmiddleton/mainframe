40a3b46c7e9674c037c7128e42f9c82cd8d14ff9
tedmiddleton <middleton.ted@gmail.com>
2023-04-28 01:18:43 +0000

    Deploying to gh-pages from @ tedmiddleton/mainframe@4a8e2b2070737ec36e9931329465087c0b8e0f5f 🚀

--- search/all_b.js
+++ search/all_b.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['operator_2b_0',['operator+',['../classmf_1_1frame.html#ab77a0c3c0245478fbbf0990602502a12',1,'mf::frame']]]
+  ['operator_2a_0',['operator*',['../classmf_1_1frame.html#a94caa80858b4cdc63da3d3a4c975e146',1,'mf::frame']]],
+  ['operator_2b_1',['operator+',['../classmf_1_1frame.html#ab77a0c3c0245478fbbf0990602502a12',1,'mf::frame']]]
 ];
