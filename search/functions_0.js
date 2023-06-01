d900807e437e3cde3af45093a75cec99fee334a0
tedmiddleton <middleton.ted@gmail.com>
2023-06-01 22:47:01 +0000

    Deploying to gh-pages from @ tedmiddleton/mainframe@ed2c45e75fc4cd922b9f168250e2db45b593be0c 🚀

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['allow_5fmissing_0',['allow_missing',['../classmf_1_1frame.html#aec1ca67f0855165deec1d07804c4029a',1,'mf::frame::allow_missing(columnindex&lt; Inds &gt;... cols) const'],['../classmf_1_1frame.html#afb953a866abd48f287bf1495b2ee0de6',1,'mf::frame::allow_missing() const']]],
-  ['append_5fcolumn_1',['append_column',['../classmf_1_1frame.html#ac856c8c99b265f47b235dbeb4efe256a',1,'mf::frame']]],
+  ['append_5fcolumn_1',['append_column',['../classmf_1_1frame.html#ac856c8c99b265f47b235dbeb4efe256a',1,'mf::frame::append_column(const std::string &amp;column_name) const'],['../classmf_1_1frame.html#a3c45601297526372ac30c918aee56b4f',1,'mf::frame::append_column(const std::string &amp;column_name, Ex expr) const']]],
   ['append_5fseries_2',['append_series',['../classmf_1_1frame.html#a82ac502f49a7c3a5d30bc941513914d6',1,'mf::frame']]]
 ];
